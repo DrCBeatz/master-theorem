2ffdc1e90d92305c2aaeaf6a84b153fb6d09fea2
DrCBeatz <brynecarruthers@gmail.com>
2024-02-19 18:51:08 -0500

    delete old plot_*.png files

--- mastertheorem/forms.py
+++ mastertheorem/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class MasterTheoremForm(forms.Form):
     a = forms.IntegerField(label='a (number of subproblems)', min_value=1)
     b = forms.IntegerField(label='b (factor by which the problem size is reduced)', min_value=2)
-    c = forms.IntegerField(label='k (exponent in the work done outside the recursive calls)', min_value=0)
+    k = forms.IntegerField(label='k (exponent in the work done outside the recursive calls)', min_value=0)
--- mastertheorem/views.py
+++ mastertheorem/views.py
@@ -1,5 +1,7 @@
 # mastertheorem/views.py
 
+import os
+from django.conf import settings
 from django.shortcuts import render
 from .forms import MasterTheoremForm
 from .master_theorem import evaluate_master_theorem, plot_master_theorem
@@ -7,17 +9,29 @@ from django.utils.crypto import get_random_string
 
 def evaluate_view(request):
     # Define default values for the form
-    default_values = {'a': 1, 'b': 2, 'c': 0}
+    default_values = {'a': 1, 'b': 2, 'k': 0}
 
     if request.method == 'POST':
         form = MasterTheoremForm(request.POST)
         if form.is_valid():
             a = form.cleaned_data['a']
             b = form.cleaned_data['b']
-            k = form.cleaned_data['c']
+            k = form.cleaned_data['k']
             complexity, case = evaluate_master_theorem(a, b, k)
 
-            # Generate a random filename for the plot
+            # Directory where plot images are saved
+            plots_directory = os.path.join(settings.BASE_DIR, 'static', 'plots')
+
+            # Delete all existing .png files in the directory
+            for filename in os.listdir(plots_directory):
+                if filename.endswith(".png"):
+                    file_path = os.path.join(plots_directory, filename)
+                    try:
+                        os.remove(file_path)
+                    except OSError as e:
+                        print(f"Error deleting file {filename}: {e.strerror}")
+
+            # Generate a new plot with a random filename
             filename = f"plot_{get_random_string(8)}.png"
             plot_url = plot_master_theorem(a, b, k, filename)
 
