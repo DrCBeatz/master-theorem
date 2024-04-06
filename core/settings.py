2426b293d26d40295c20db7a94f84173b27676e0
DrCBeatz <brynecarruthers@gmail.com>
2024-04-05 22:13:34 -0400

    add internal IP address env variable to ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -15,12 +15,13 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = [
-    '*',
-    # "localhost",
-    # "127.0.0.1",
-    # "testserver",
-    # "www.mastertheorem.io",
-    # "mastertheorem-aws-dev.us-west-2.elasticbeanstalk.com"
+    # '*',
+    "localhost",
+    "127.0.0.1",
+    "testserver",
+    "www.mastertheorem.io",
+    "mastertheorem-aws-dev.us-west-2.elasticbeanstalk.com"
+    env.("INTERNAL_IP_ADDRESS", default=""),
 ]
 
 
