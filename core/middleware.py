76bee9318a37c4bb9092dfbdde90d4876bfa9dff
DrCBeatz <brynecarruthers@gmail.com>
2024-04-14 17:56:19 -0400

    update ELBHealthCheckMiddleware to check ip address of network

--- core/middleware.py
+++ core/middleware.py
@@ -1,16 +1,21 @@
-from django.http import HttpResponseForbidden
-from django.conf import settings
+from django.http import HttpResponse
+from ipaddress import ip_address, ip_network
 
 class ELBHealthCheckMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-    
+
     def __call__(self, request):
-        # All all requests from the ELB Health Checker
-        if 'ELB-HealthChecker/2.0' in request.META.get('HTTP_USER_AGENT', ''):
-            return self.get_response(request)
-        # Validate against ALLOWED_HOSTS
-        host = request.get_host().split(':')[0]
-        if host not in settings.ALLOWED_HOSTS:
-            return HttpResponseForbidden('Host not allowed')
-        return self.get_response(request)
+        # Define the allowed network
+        allowed_network = ip_network("172.31.0.0/16")
+
+        # Get the client's IP address from the request
+        client_ip = ip_address(request.META.get("REMOTE_ADDR"))
+
+        # Check if the client IP is in the allowed network
+        if client_ip in allowed_network:
+            # Mark the host as allowed
+            request.META['HTTP_HOST'] = request.get_host()
+
+        response = self.get_response(request)
+        return response
