c528bf7ace1e753c0ca75aed8ec8ea4f80b9082d
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-05-03 21:37:57 -0400

    Clean up vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/static/",
   plugins: [react()],
